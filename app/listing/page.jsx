698e64dd162e43a4b7dbfbd2b4c2ee852a52cc26
Aditya-Mishra799 <adityamishra@eng.rizvi.edu.in>
2024-03-17 12:14:05 +0530

    Trying to fix prerendering error attempt 2

--- app/listing/page.jsx
+++ app/listing/page.jsx
@@ -2,21 +2,15 @@
 import React, { useEffect, useState } from "react";
 import { useSearchParams, usePathname, useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
-
+import { fetchListingData } from "@utils/fetchListingData";
 const page = () => {
   const { data: session } = useSession();
   const router = useRouter();
   const listing_id = useSearchParams().get("id");
   const [listingData, setListingData] = useState()
-  const fetchListingData = async()=>{
-    const endpoint = process.env.NEXT_PUBLIC_URL+'/api/listing/view/'+listing_id
-    console.log(endpoint)
-    const response = await fetch(endpoint);
-    const data = await response.json()
-    setListingData(data)
-  }
+  
   useEffect(()=>{
-    fetchListingData()
+    fetchListingData(setListingData, listing_id)
   },[])
   return <div>
     Listing Data : 
