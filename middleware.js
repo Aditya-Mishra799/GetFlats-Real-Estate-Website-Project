7f0a8e91ac27f2f25eadf5bb0cd51537e934abd0
Aditya-Mishra799 <adityamishra@eng.rizvi.edu.in>
2024-03-18 10:01:55 +0530

    fixing error

--- middleware.js
+++
@@ -1,8 +0,0 @@
-export { default } from "next-auth/middleware"
-import { NextResponse } from "next/server"
-export function middleware(req){
-    const sessionToken = req.cookies.get('next-auth.session-token')?.value
-    if (!sessionToken) return NextResponse.redirect(new URL('/auth', req.url))
-    return 
-}
-// export const config = { matcher: ["/profile/:path*"] }
