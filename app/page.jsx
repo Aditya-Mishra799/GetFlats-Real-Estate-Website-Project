d89af7dc5dc5e124ffd5b1d9ae33be03f700b01d
Aditya-Mishra799 <adityamishra@eng.rizvi.edu.in>
2024-03-21 21:32:12 +0530

    added file image upload path

--- app/page.jsx
+++ app/page.jsx
@@ -15,6 +15,7 @@ const Page = () => {
       label: "view profile",
     },
   };
+ 
   useEffect(() => {
     const hasRunOnce = sessionStorage.getItem("hasRunOnce");
     if (!hasRunOnce && session?.user) {
