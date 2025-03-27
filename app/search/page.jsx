5f964ad4508daf2f77eb59a5f45bc45b33207a25
Aditya-Mishra799 <adityamishra@eng.rizvi.edu.in>
2025-03-27 15:14:20 +0530

    Added loading fallback in search page

--- app/search/page.jsx
+++ app/search/page.jsx
@@ -1,5 +1,5 @@
 "use server";
-import React from "react";
+import React, { Suspense } from "react";
 import { connectToDB } from "@utils/database";
 import PropertyListing from "@models/property_listing";
 import PropertyListingsCard from "@components/PropertyLisingsCard";
@@ -10,6 +10,7 @@ import User from "@models/user";
 import { getServerSession } from "next-auth/next";
 import { nextAuthOptions } from "@app/api/auth/[...nextauth]/route";
 import { checkForFavourites } from "@app/api/listing/user/[id]/route";
+import PropertyCardLoadingSkeleton from "@components/PropertyCardLoadingSkeleton";
 
 const buildMongoDBSearchQuery = (filterQuery) => {
   const queryArrayAndPart = [];
@@ -142,9 +143,35 @@ const page = async ({ searchParams }) => {
 
   const perPage = 8;
 
+  const fallbackComp = (
+    <div className="flex  gap-4  flex-wrap  mx-8 lg:mx-14 ">
+      {Array(8)
+        .fill(null)
+        .map((_, index) => (
+          <PropertyCardLoadingSkeleton key={index} />
+        ))}
+    </div>
+  );
+  return (
+    <div className="container  mt-8 w-full">
+      <div className="flex  gap-1">
+        <div className="lg:max-w-96">
+          <FilterPanel query={query} />
+        </div>
+        <div className="grow w-full" key = {query} >
+          {/* Render fallback until the query data fetch request is completed */}
+          <Suspense fallback={fallbackComp} key = {query}>
+            <RenderListingsData key = {query} page={page} perPage={perPage} query={query} />
+          </Suspense>
+        </div>
+      </div>
+    </div>
+  );
+};
+// seperate component to render data only when the data is received
+const RenderListingsData = async ({ page, perPage, query }) => {
   //fetch the data based on page and our filter query
   const data = await getData(perPage, page, query);
-
   //user navigation between pages
   const prevPage = page - 1 > 0 ? page - 1 : 1;
   const nextPage = page + 1;
@@ -159,61 +186,54 @@ const page = async ({ searchParams }) => {
     }
   }
   return (
-    <div className="container  mt-8 w-full">
-      <div className="flex  gap-1">
-        <div className="lg:max-w-96">
-          <FilterPanel query={query} />
-        </div>
-        <div className="grow w-full">
-          <h3 className="text-2xl font-semibold mx-8 lg:mx-14 mb-4 text-slate-800">
-            Showing {data.listingCount === 0 ? 0 : (page - 1) * perPage + 1} -{" "}
-            {page === totalPages || data.listingCount === 0
-              ? data.listingCount
-              : page * perPage}{" "}
-            listings out of {data.listingCount}
-          </h3>
-          <div className="flex  gap-4  flex-wrap  mx-8 lg:mx-14 ">
-            {data.listingCount === 0 && (
-              <div className="my-6 text-xl font-bold w-full text-center text-slate-600">
-                No listings found matching with the configured filter : (
-              </div>
-            )}
-            {JSON.parse(data.listings).map((listing) => (
-              <PropertyListingsCard data={listing} key={listing._id} />
-            ))}
+    <>
+      <h3 className="text-2xl font-semibold mx-8 lg:mx-14 mb-4 text-slate-800">
+        Showing {data.listingCount === 0 ? 0 : (page - 1) * perPage + 1} -{" "}
+        {page === totalPages || data.listingCount === 0
+          ? data.listingCount
+          : page * perPage}{" "}
+        listings out of {data.listingCount}
+      </h3>
+      <div className="flex  gap-4  flex-wrap  mx-8 lg:mx-14 ">
+        {data.listingCount === 0 && (
+          <div className="my-6 text-xl font-bold w-full text-center text-slate-600">
+            No listings found matching with the configured filter : (
           </div>
+        )}
+        {JSON.parse(data.listings).map((listing) => (
+          <PropertyListingsCard data={listing} key={listing._id} />
+        ))}
+      </div>
 
-          <div className="flex justify-center items-center mt-10 mb-5">
-            <div className="flex border gap-1 rounded-lg border-faded-orange px-2.5 py-1.5 text-md font-semibold">
-              {page === 1 ? (
-                <div className="opacity-65 text-slate-400 ">Previous</div>
-              ) : (
-                <Link href={`?page=${prevPage}&query=${query}`}>Previous</Link>
-              )}
+      <div className="flex justify-center items-center mt-10 mb-5">
+        <div className="flex border gap-1 rounded-lg border-faded-orange px-2.5 py-1.5 text-md font-semibold">
+          {page === 1 ? (
+            <div className="opacity-65 text-slate-400 ">Previous</div>
+          ) : (
+            <Link href={`?page=${prevPage}&query=${query}`}>Previous</Link>
+          )}
 
-              {pageNumbers.map((pageNumber, index) => (
-                <Link
-                  href={`?page=${pageNumber}&query=${query}`}
-                  className={`${
-                    pageNumber === page
-                      ? "text-white bg-active-orange  "
-                      : "hover:bg-faded-orange"
-                  } px-2 rounded-sm transition-all`}
-                  key={index}
-                >
-                  {pageNumber}
-                </Link>
-              ))}
-              {page === totalPages ? (
-                <div className="opacity-65  text-slate-400">Next</div>
-              ) : (
-                <Link href={`?page=${nextPage}&query=${query}`}>Next</Link>
-              )}
-            </div>
-          </div>
+          {pageNumbers.map((pageNumber, index) => (
+            <Link
+              href={`?page=${pageNumber}&query=${query}`}
+              className={`${
+                pageNumber === page
+                  ? "text-white bg-active-orange  "
+                  : "hover:bg-faded-orange"
+              } px-2 rounded-sm transition-all`}
+              key={index}
+            >
+              {pageNumber}
+            </Link>
+          ))}
+          {page === totalPages ? (
+            <div className="opacity-65  text-slate-400">Next</div>
+          ) : (
+            <Link href={`?page=${nextPage}&query=${query}`}>Next</Link>
+          )}
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
